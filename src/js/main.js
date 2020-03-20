e1d46cf105605c70e5b3a5cf068f8eb7e5fa2727
Bogdan Dolin <d.bogdan@inbox.ru>
2020-03-20 19:45:34 +0700

    Finish up the pages.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,31 @@
 $(function () {
 
+  var windowWidth = $(window).width();
+
   var promoSection = $(".js-promo");
   var $scrolldownTrigger = $('.scrolldown-trigger');
   var $aboutUsSection = $('.js-about-us');
   var $aboutUsPageSection = $('.js-about-us-page');
+  var $servicesSection = $('.js-services');
+  var $projectsSection = $('.js-projects');
+  var $newsSection = $('#scroll-news');
+  var $contactsSection = $('.js-contacts');
+  var $innerPageHeader = $('.header--inner');
+  var projectsSwiper;
 
   var $mobileNavTrigger = $('.mobile-nav-trigger');
   var mobileMenuIsVisible = false;
   var mobileMenuTimeline = new TimelineMax({paused: true});
 
-  var callbackPopupTimeline = new TimelineMax({paused: true});
+  var phoneMaskInitialized = false;
+  var callbackPopupTimeline = new TimelineMax({
+    paused: true, onComplete: function () {
+      if (!phoneMaskInitialized) {
+        $(".js-phone-field").inputmask("8 (999) 999-99-99");
+      }
+      phoneMaskInitialized = true;
+    }
+  });
   var $callbackPopupTrigger = $('.js-callback-popup-trigger');
   var callbackPopupIsVisible = false;
 
@@ -23,8 +39,11 @@ $(function () {
   // Анимация футера
   animateFooterOnScroll();
 
+  if (isIE()) {
+    $('body').addClass('ie');
+  }
   // Запускаем анимацию на главной
-  if (promoSection.length > 0) {
+  if (promoSection.length > 0 && !isIE()) {
     animatePromoSection();
     initPromoScrollScene();
   }
@@ -35,14 +54,35 @@ $(function () {
   }
 
   // Анимация секции "О нас" на главной
-  if ($aboutUsSection.length > 0) {
+  if ($aboutUsSection.length > 0 && windowWidth >= 1000) {
     animateAboutSection();
   }
 
+  // Анимация секции "Услуги" на главной
+  if ($servicesSection.length > 0 && windowWidth >= 1000) {
+    animateServicesSection();
+  }
+
   // Анимация секции "О нас" на отдельной странице
-  if ($aboutUsPageSection.length > 0) {
+  if ($aboutUsPageSection.length > 0 && windowWidth >= 1000) {
     animateAboutPageSection();
   }
+
+  // Анимация секции "Проекты" на главной
+  if ($projectsSection.length > 0 && windowWidth >= 1000) {
+    animateProjectsSection();
+  }
+
+  // Анимация секции "Новости" на главной
+  if ($newsSection.length > 0 && windowWidth >= 1000) {
+    animateNewsSection();
+  }
+
+  // Анимация секции "Контакты" на главной
+  if ($contactsSection.length > 0 && windowWidth >= 1000) {
+    animateContactsSection();
+  }
+
 
   // Инициализация скролла
   initScrollTo();
@@ -69,13 +109,23 @@ $(function () {
   if ($callbackPopupTrigger.length > 0) {
 
     callbackPopupTimeline
-      .set(".popup-callback", { display: 'block'})
+      .set(".popup-callback", {display: 'block'})
       .fromTo('.popup__overlay', 0.3, {xPercent: 100}, {xPercent: 0})
       .fromTo('.popup__skew', 0.3, {xPercent: 100}, {xPercent: 0}, "-=0.2")
       .staggerFromTo('.popup__title, .form-row', 0.3, {x: 30, opacity: 0}, {x: 0, opacity: 1}, 0.1);
-      // .fromTo('.mobile-menu .social-icons', 0.2, {y: 30, opacity: 0}, {y: 0, opacity: 1});
+    // .fromTo('.mobile-menu .social-icons', 0.2, {y: 30, opacity: 0}, {y: 0, opacity: 1});
 
     $callbackPopupTrigger.on('click', onCallbackPopupTriggerClick);
+  }
+
+  // Анимация лого на внутренних страницах
+  if ($innerPageHeader.length > 0) {
+    animateInnerPageLogo();
+  }
+
+  // Делаем анимацию линий на внутренних страницах
+  if ($innerPageHeader.length > 0 && windowWidth >= 1000) {
+    animateInnerPageLines();
   }
 
   function onMobileNavTriggerClick(e) {
@@ -103,23 +153,27 @@ $(function () {
     }
 
     $('.js-callback-popup-close').on('click', onCallbackPopupTriggerClick);
-    $('.popup__overlay').on('click', function() {
+
+    $('.popup__overlay').on('click', function () {
       callbackPopupTimeline.reverse(null, true);
       callbackPopupIsVisible = false;
     });
 
   }
+
   function animateScrolldownTrigger() {
     var tl = new TimelineMax({repeat: -1, repeatDelay: 3});
 
-    tl
-      .to($scrolldownTrigger, 0.1, {y: "20"})
-      .to($scrolldownTrigger, 0.05, {y: "0"}, "+=0.2")
-      .to($scrolldownTrigger, 0.1, {y: "20"}, "+=0.2")
-      .to($scrolldownTrigger, 0.05, {y: "0"}, "+=0.2");
+    // tl
+    //   .to($scrolldownTrigger, 0.1, {y: "20"})
+    //   .to($scrolldownTrigger, 0.05, {y: "0"}, "+=0.2")
+    //   .to($scrolldownTrigger, 0.1, {y: "20"}, "+=0.2")
+    //   .to($scrolldownTrigger, 0.05, {y: "0"}, "+=0.2");
+    //
 
     $scrolldownTrigger.on('click', function (e) {
       e.preventDefault();
+      TweenMax.to(window, 2, {scrollTo: '#scroll-about-us'});
     });
   }
 
@@ -134,8 +188,7 @@ $(function () {
         {attr: {gradientTransform: 'translate(-1,1)'}},
         {attr: {gradientTransform: 'translate(3,1)'}, repeat: -1, repeatDelay: 4}, '-=1')
       .fromTo('.header--primary', 0.3, {opacity: 0}, {opacity: 1}, '-=3')
-      .fromTo('.promo__bottom', 0.3, {opacity: 0}, {opacity: 1}, '-=3');
-
+      .fromTo('.promo__bottom', 0.3, {opacity: 0}, {opacity: 1}, '-=0.3');
   }
 
   function initPromoScrollScene() {
@@ -166,12 +219,178 @@ $(function () {
 
     var tl = new TimelineMax();
 
-    tl.fromTo('.js-rocket-progress', 1, {xPercent: -100}, {xPercent: 0});
+    tl.staggerFromTo('#rect-blue-1, #rect-white-1, #rect-blue-2, #rect-blue-3, #rect-red-1', 1, {
+      x: 100,
+      opacity: 0
+    }, {x: 0, opacity: 1}, 0.3)
+      .staggerFromTo('.about-us__title, .about-us__subtitle, .about-us__button-wrap', 1, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.1, '-=1.5')
+      .fromTo('.about-us__line', 1, {
+        opacity: 0,
+        height: 0,
+      }, {height: '95%', opacity: 1}, '-=0.2')
+      .fromTo('.about-us__fighters', 1, {
+        opacity: 0,
+        x: 100,
+      }, {x: 0, opacity: 1})
+      .staggerFromTo('.about-us .map-marker', 1, {
+        opacity: 0,
+        y: 50,
+      }, {y: 0, opacity: 1}, 0.2, '-=1')
+      .fromTo('.about-us .map-text', 1, {
+        opacity: 0,
+        x: 100,
+      }, {x: 0, opacity: 1}, '-=2');
+
+    var aboutScene = new ScrollMagic.Scene({
+      triggerElement: '.js-about-us',
+      triggerHook: 1,
+      duration: '160%'
+    })
+      .setTween(tl)
+      .addTo(controller);
+
+    // Анимация ракеты
+    var rocketTimeline = new TimelineMax();
+
+    rocketTimeline.fromTo('.js-rocket-progress', 2, {xPercent: -100}, {xPercent: 0});
 
     var secondPromoScene = new ScrollMagic.Scene({
-      triggerElement: '#services',
+      triggerElement: '#scroll-services',
       triggerHook: 1,
-      duration: '100%'
+      duration: 0,
+    })
+      .setTween(rocketTimeline)
+      .addTo(controller);
+  }
+
+
+  function animateServicesSection() {
+
+    var tl = new TimelineMax();
+
+    tl
+      .staggerFromTo('.services__title-word, .services__title-text', 1, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.7)
+      .staggerFromTo('.service-step', 0.3, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.3)
+      .fromTo('.services-steps__line', 2.4, {
+        height: 0,
+        opacity: 0
+      }, {height: '90%', opacity: 1}, "-=2.4")
+      .staggerFromTo('#rect-services-blue-1,#rect-services-blue-2,#rect-services-blue-3', 0.6, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.3)
+      .staggerFromTo('.services__fighters', 0.3, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.3);
+    var aboutScene = new ScrollMagic.Scene({
+      triggerElement: '.js-services',
+      triggerHook: 0.6,
+      duration: 0
+    })
+      .setTween(tl)
+      .addTo(controller);
+  }
+
+  function animateProjectsSection() {
+
+    var tl = new TimelineMax({
+      onComplete: function () {
+        projectsSwiper.autoplay.start();
+        projectsSwiper.autoplay.disableOnInteraction = false;
+      }
+    });
+
+    tl
+      .fromTo('#projects', 1, {opacity: 0}, {opacity: 1})
+      .staggerFromTo('.projects__title-word, .projects__title-text', 1, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 1)
+      .staggerFromTo('#rect-blue-projects-1, #rect-white-projects-1, #rect-red-projects-1, #rect-blue-projects-2, #rect-red-projects-2, #rect-white-projects-2, #rect-news-blue-1', 0.3, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.2)
+      .fromTo('.projects-slider', 1, {opacity: 0}, {opacity: 1})
+      .fromTo('.projects-navigation', 1, {opacity: 0}, {opacity: 1});
+
+    var projects = new ScrollMagic.Scene({
+      triggerElement: '#projects',
+      triggerHook: 0.8,
+      duration: 0,
+      reverse: false,
+    })
+      .setTween(tl)
+      .addTo(controller);
+  }
+
+  function animateNewsSection() {
+
+    var tl = new TimelineMax();
+
+    tl
+      .staggerFromTo('.news__title-word, .news__title-text, .news__button-wrap', 1, {
+        x: 100,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 1)
+      .staggerFromTo('.news .swiper-slide-visible', 1, {
+        y: 40,
+        opacity: 0
+      }, {y: 0, opacity: 1}, 0.2);
+
+    var news = new ScrollMagic.Scene({
+      triggerElement: '#scroll-news',
+      triggerHook: 0.8,
+      duration: 0,
+      reverse: false,
+    })
+      .setTween(tl)
+      .addTo(controller);
+  }
+
+  function animateContactsSection() {
+
+    var tl = new TimelineMax();
+
+    tl
+      .staggerFromTo('#rect-contacts-white-1, #rect-footer-red-1, #rect-footer-blue-1, #rect-footer-white-1, .contacts__fighter', 0.3, {
+        x: 30,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.3)
+      .fromTo('.contacts__line', 1, {
+        height: 0,
+        opacity: 0
+      }, {height: 212, opacity: 1})
+      .fromTo('.contacts__top .section-title', 1, {
+        x: 30,
+        opacity: 0
+      }, {x: 0, opacity: 1})
+      .staggerFromTo('.contacts__list li', 0.3, {
+        x: 30,
+        opacity: 0
+      }, {x: 0, opacity: 1}, 0.3)
+      .fromTo('.contact-callback', 1, {
+        opacity: 0
+      }, {opacity: 1});
+    // .staggerFromTo('.news .swiper-slide-visible', 1, {
+    //   y: 40,
+    //   opacity: 0
+    // }, {y: 0, opacity: 1}, 0.2);
+
+    var contacts = new ScrollMagic.Scene({
+      triggerElement: '.js-contacts',
+      triggerHook: 0.7,
+      duration: 0,
+      reverse: false,
     })
       .setTween(tl)
       .addTo(controller);
@@ -195,8 +414,19 @@ $(function () {
   function initSliders() {
 
     // Слайдер проектов
-    var projectsSwiper = new Swiper('.projects-slider', {
+    projectsSwiper = new Swiper('.projects-slider', {
       spaceBetween: 200,
+      speed: 1000,
+      // loop: true,
+      // loopAdditionalSlides: true,
+      watchSlidesVisibility: true,
+      breakpoints: {
+        1000: {
+          autoplay: {
+            delay: 5000,
+          }
+        }
+      },
       // loop: true,
       navigation: {
         prevEl: '.projects-navigation__arrow--prev',
@@ -208,6 +438,7 @@ $(function () {
     var newsSwiper = new Swiper('.news-slider', {
       spaceBetween: 1,
       slidesPerView: 3,
+      watchSlidesVisibility: true,
       loop: true,
       breakpoints: {
         310: {
@@ -232,7 +463,7 @@ $(function () {
     var secondPromoScene = new ScrollMagic.Scene({
       triggerElement: '.footer',
       triggerHook: 1,
-      duration: $('.footer').height()
+      duration: 0,
     })
       .setTween(tl)
       .addTo(controller);
@@ -302,7 +533,6 @@ $(function () {
       },
     });
 
-
     var infoPartnersSlider = new Swiper('.info-partners-slider', {
       slidesPerView: 1,
       watchOverflow: true,
@@ -328,4 +558,35 @@ $(function () {
       },
     });
   }
+
+  function animateInnerPageLogo() {
+    var tl = new TimelineMax();
+
+    tl
+      .fromTo('#header-logo-linear-gradient',
+        4,
+        {attr: {gradientTransform: 'translate(-1,1)'}},
+        {attr: {gradientTransform: 'translate(3,1)'}, repeat: -1, repeatDelay: 4, delay: 2}, '-=1');
+  }
+
+  function animateInnerPageLines() {
+    var tl = new TimelineMax();
+
+    var $rects = $('.rect');
+
+    if ($rects.length > 0) {
+      tl
+        .staggerFromTo($rects, 0.3, {opacity: 0}, {opacity: 1}, 0.3);
+    }
+
+  }
+
+  function isIE() {
+    if (navigator.appName === 'Microsoft Internet Explorer' || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 });
